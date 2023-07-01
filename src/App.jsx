9bd5ae8495b58b5931e95caa09807dfb2819b1d6
leocsdev <leocsdev@gmail.com>
2023-07-01 23:37:19 +0800

    First commit, initialized react app with tailwindcss.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <section className='body-font font-poppins'>
+      <h1 className='text-2xl font-bold text-center'>
+        Hello Age Calculator App
+      </h1>
+    </section>
+  );
+}
+
+export default App;
