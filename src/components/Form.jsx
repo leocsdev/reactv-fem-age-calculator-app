dfd1a887b29ac9a2ba91c1e5046332fa501bf126
leocsdev <leocsdev@gmail.com>
2023-07-02 18:12:27 +0800

    Added more feature to on the date validation. It now checks if the date exceeds the current date.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,46 @@
 import { useForm } from 'react-hook-form';
-import { getDay, getMonth, getYear } from '../../lib/dateHelper';
+import {
+  getCurrentDay,
+  getCurrentMonth,
+  getCurrentYear,
+} from '../../lib/dateHelper';
 
 export default function Form({ getDate }) {
-  const currentYear = getYear();
-  const currentMonth = getMonth();
-  const currentDay = getDay();
+  const currentYear = getCurrentYear();
+  const currentMonth = getCurrentMonth();
+  const currentDay = getCurrentDay();
 
   const {
     register,
     handleSubmit,
+    setValue,
+    getValues,
     formState: { errors },
   } = useForm();
 
   const onSubmit = (data) => {
     const date = `${data.year}-${data.month}-${data.day}`;
     getDate(date);
+  };
+
+  const handleYearChange = (e) => {
+    const year = parseInt(e.target.value);
+    setValue('year', year);
+
+    const month = parseInt(getValues('month'));
+    const day = parseInt(getValues('day'));
+
+    // If current year, the months and days should not
+    // exceed the current month and day.
+    if (year === currentYear) {
+      if (month >= currentMonth) {
+        setValue('month', currentMonth);
+
+        if (day > currentDay) {
+          setValue('day', currentDay);
+        }
+      }
+    }
   };
 
   return (
@@ -37,10 +63,15 @@ export default function Form({ getDate }) {
           type='number'
           placeholder='MM'
           className='border'
-          {...register('month', { required: true, min: 1, max: 12 })}
+          {...register('month', {
+            required: true,
+            min: 1,
+            max: 12,
+          })}
         />
         {errors.month?.type === 'required' && <p>This field is required</p>}
         {errors.month?.type === 'min' && <p>Must be a valid month</p>}
+        {errors.month?.type === 'max' && <p>Must be a valid month</p>}
       </div>
       <div className='flex flex-col items-start pb-2'>
         <label htmlFor='year'>Year</label>
@@ -53,6 +84,7 @@ export default function Form({ getDate }) {
             min: 100,
             max: currentYear,
           })}
+          onChange={handleYearChange}
         />
         {errors.year?.type === 'required' && <p>This field is required</p>}
         {errors.year?.type === 'min' && <p>Must be a valid year</p>}
