c3a305e7c720a2515ff263cae9c8a9f0cb1bd108
leocsdev <leocsdev@gmail.com>
2023-07-03 18:21:52 +0800

    Added colors from the design.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        // Primary
+        purple: 'hsl(259, 100%, 65%)',
+        lightRed: 'hsl(0, 100%, 67%)',
+        // Neutral
+        offWhite: 'hsl(0, 0%, 94%)',
+        lightGrey: 'hsl(0, 0%, 86%)',
+        smokeGrey: 'hsl(0, 1%, 44%)',
+        offBlack: 'hsl(0, 0%, 8%)',
+      },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
