a0a2f494a6ac6501700d5f3044ab7abc2ab269a8
leocsdev <leocsdev@gmail.com>
2023-07-03 21:54:17 +0800

    Added active and focus state in Form component.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -114,8 +114,8 @@ export default function Form({ setDate }) {
             placeholder='DD'
             className={
               ['required', 'min', 'max'].includes(errors.day?.type)
-                ? 'border border-red-400 w-full font-bold text-xl px-4 py-2 rounded-md md:text-3xl md:rounded-lg'
-                : 'border w-full font-bold text-xl px-4 py-2 rounded-md md:text-3xl md:rounded-lg'
+                ? 'border border-red-400 w-full font-bold text-xl px-4 py-2 rounded-md md:text-3xl md:rounded-lg active:outline-none focus:outline-none'
+                : 'border w-full font-bold text-xl px-4 py-2 rounded-md md:text-3xl md:rounded-lg active:outline active:outline-purple-500 focus:outline focus:outline-purple-500 hover:outline-purple-500'
             }
             {...register('day', { required: true, min: 1, max: maxDays })}
             onChange={handleDayChange}
@@ -153,8 +153,8 @@ export default function Form({ setDate }) {
             placeholder='MM'
             className={
               ['required', 'min', 'max'].includes(errors.day?.type)
-                ? 'border border-red-400 w-full font-bold text-xl px-4 py-2 rounded-md md:text-3xl md:rounded-lg'
-                : 'border w-full font-bold text-xl px-4 py-2 rounded-md md:text-3xl md:rounded-lg'
+                ? 'border border-red-400 w-full font-bold text-xl px-4 py-2 rounded-md md:text-3xl md:rounded-lg active:outline-none focus:outline-none'
+                : 'border w-full font-bold text-xl px-4 py-2 rounded-md md:text-3xl md:rounded-lg active:outline active:outline-purple-500 focus:outline focus:outline-purple-500 hover:outline-purple-500'
             }
             {...register('month', {
               required: true,
@@ -196,8 +196,8 @@ export default function Form({ setDate }) {
             placeholder='YYYY'
             className={
               ['required', 'min', 'max'].includes(errors.day?.type)
-                ? 'border border-red-400 w-full font-bold text-xl px-4 py-2 rounded-md md:text-3xl md:rounded-lg'
-                : 'border w-full font-bold text-xl px-4 py-2 rounded-md md:text-3xl md:rounded-lg'
+                ? 'border border-red-400 w-full font-bold text-xl px-4 py-2 rounded-md md:text-3xl md:rounded-lg active:outline-none focus:outline-none'
+                : 'border w-full font-bold text-xl px-4 py-2 rounded-md md:text-3xl md:rounded-lg active:outline active:outline-purple-500 focus:outline focus:outline-purple-500 hover:outline-purple-500'
             }
             {...register('year', {
               required: true,
